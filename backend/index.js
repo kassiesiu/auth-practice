72aaf825192b15f249d96abb95be25610132c1fe
Davis <davis.nelson27@gmail.com>
2021-10-03 14:22:03 -0700

    Added response object from backend + grammer issue fix

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,20 @@
 const express = require('express');
-
+// const cors = require('cors');
 const app = express();
 const port = 8080;
 
+// const cors = {
+//     origin: "https://backend.auth-practice.com"
+// }
+
+const resObj = {
+    "message": "I am Alive!"
+};
+
 app.get('/healthcheck', (req, res) => {
-    res.send("I am Alive!!");
+    res.send(resObj);
 });
 
 app.listen(port, () => {
-    console.log("Server Started....")
-})
+    console.log("Server Started...")
+});
