f2be60c3d5b13a7998c10a83f43ccfc34bf72afb
Davis <davis.nelson27@gmail.com>
2021-10-03 16:40:43 -0700

    Fixed CORS

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,15 @@
 const express = require('express');
-// const cors = require('cors');
+const cors = require('cors');
 const app = express();
 const port = 8080;
-
-const corsOptions = {
-    origin: "http://localhost:3000"
-}
 
 const resObj = {
     "message": "I am Alive!"
 };
 
+app.use(cors())
 
-app.get('/healthcheck', cors(corsOptions), (req, res) => {
+app.get('/healthcheck', (req, res) => {
     res.send(resObj);
 });
 
