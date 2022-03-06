327eea41670db0a57c9b679f3e6aec6d8c6a628d
kassiesiu <ksiuwng@gmail.com>
2022-03-06 13:38:57 -0800

    add login

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -4,16 +4,29 @@ import { Input, Button } from 'antd';
 import { Link  } from "react-router-dom";
 
 class Login extends React.Component {
-    async login() {
-        const thing = await fetch('http://localhost:8081/healthcheck')
+    state = {
+        username: '',
+        password: '',
+    }
+
+    login = async () => {
+        const {username, password} = this.state;
+        const thing = await fetch('http://localhost:8081/login', {
+            method: 'POST',
+            body: JSON.stringify({username, password})
+        })
         console.log('thing :>> ', await thing.json());
+    }
+
+    onChange = (e) => {
+        this.setState({[e.target.name]: e.target.value})
     }
 
     render() {
         return (
             <div className="Login">
-                    <Input className="child" placeholder="Username"></Input>
-                    <Input className="child" placeholder="Password"></Input>
+                    <Input className="child" placeholder="Username" name="username" onChange={this.onChange}/>
+                    <Input className="child" placeholder="Password" name="password" onChange={this.onChange}/>
                     <Button  className="child" type="primary" onClick={this.login}>Log In</Button>
                     <span className="child">Not registered yet? <Link to="/register">Create an account</Link></span>
             </div>
